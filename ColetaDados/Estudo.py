89a7795c588ecf0c478ea0e015b3203c8ada1f01
Wellington Guimaraes <112776793+WellyngthonG@users.noreply.github.com>
2024-12-15 11:20:01 -0300

    Add files via upload
    Tarefas das aulas

---
+++ ColetaDados/Estudo.py
@@ -0,0 +1,11 @@
+# Entendimento da linha print(response.text[:600])
+texto = "Extrair msg de texto"
+
+subtexto = texto[:11]
+print(subtexto)
+
+subtexto = texto[8:]
+print(subtexto)
+
+subtexto = texto[-5:]
+print(subtexto)
